6c01a8ba2f0e1d7171a8979c47f0a89af5247fe7
MLRichter <matrichter@uos.de>
2022-11-13 00:43:14 -0500

    first

--- mobilenetv2.py
+++ mobilenetv2.py
@@ -3,7 +3,20 @@ from typing import Any, Callable, List, Optional
 import torch
 from torch import nn, Tensor
 
-from torchvision.models._utils import _make_divisible
+def _make_divisible(v: float, divisor: int, min_value: Optional[int] = None) -> int:
+    """
+    This function is taken from the original tf repo.
+    It ensures that all layers have a channel number that is divisible by 8
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    """
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
 
 
 class ConvNormActivation(torch.nn.Sequential):
